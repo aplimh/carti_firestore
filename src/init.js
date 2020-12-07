a0f513411608a34d1b7a3f8ed35ceec344caed83
Mihai Damian <iap.aplimh@gmail.com>
2020-12-07 19:16:25 +0200

    Update init.js

--- src/init.js
+++ src/init.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdXOoEq_Pkm43q7Kk3i6DFmSi4",
-  authDomain: "carti-2b007.firebaseapp.com",
-  projectId: "carti-2b003",
-  storageBucket: "carti-2b003.appspot.com",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "carti-xxxxxx.firebaseapp.com",
+  projectId: "carti-xxxxxx",
+  storageBucket: "carti-xxxxxx.appspot.com",
   messagingSenderId: "106980953060",
-  appId: "1:106980953060:web:b48ce626882fcec13f95f1",
+  appId: "1:106980953060:web:xxxxxxxxxxxxxxxxxxxxxxxx",
 };
 
 // Initialize Firebase
